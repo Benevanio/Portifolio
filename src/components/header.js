041d09d1f5064de62adc9e3b8301a74169786762
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-01 22:48:19 -0300

    add text

--- src/components/header.js
+++ src/components/header.js
@@ -7,6 +7,7 @@ function header() {
   return (
     <div className="heaper-warapper">
     <main className="main-info">
+      <div className="main-info-text">Welcome to my portfolio</div>
        <ReactTypingEffect className="typingeffect" text={['I am Bene Tesla','Frontend developer','Backend developer',
         'Mobile developer',      
        ]} speed={100} eraseDelay={500}/>
