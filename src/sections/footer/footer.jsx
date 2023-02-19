e6da51c21a1307af37f1037f69ca54f3d11737b2
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-19 18:02:33 -0300

    Acabando o rodape da aplicacao

--- src/sections/footer/footer.jsx
+++ src/sections/footer/footer.jsx
@@ -3,7 +3,33 @@ import './footer.scss'
 const footer = () => {
   return (
     <section id='footer'>
-      <h2>Footer</h2>
+      <div className='container footer__container'>
+        <div className='footer__social'>
+          <a href='https://www.linkedin.com/in/bene-tesla/' target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-linkedin-in'></i>
+          </a>
+          <a href='https://github.com/benetesla' target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-github'></i>
+          </a>
+          <a href='https://www.instagram.com/bene_tesla/' target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-instagram'></i>
+          </a>
+        
+        </div>
+        <p className='footer__text'>
+          Made with <i className='fas fa-heart'></i> by Bene Tesla
+        </p>
+        <p className='footer__text'>
+          © 2020 All rights reserved
+        </p>
+        <p className='footer__text'>
+        buy me a coffee <a href='https://www.buymeacoffee.com/BeneTesla' target='_blank' rel='noopener noreferrer'>
+            <i className='fas fa-coffee'></i>
+        </a>
+        </p>
+
+      </div>
+
     </section>
   )
 }
