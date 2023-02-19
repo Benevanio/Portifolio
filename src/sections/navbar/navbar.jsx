ee8477332a42935db8154c44ddc30a5462a4fa56
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-19 13:08:16 -0300

    Logo via path(Caminho)

--- src/sections/navbar/navbar.jsx
+++ src/sections/navbar/navbar.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
+import Logo  from '../../images/Logo.jpg'
 import './navbar.scss'
 const navbar = () => {
   return (
     <nav id='nav'>
       <div className='container nav__container'>
         <a href='#' className='nav__logo'>
-          <img src='https://user-images.githubusercontent.com/78994881/219959728-52fc0a6a-816a-46a7-9046-c699fbb36003.jpg' alt='' />
+          <img src={Logo} alt='logo' />
         </a>      
       </div>
     </nav>
