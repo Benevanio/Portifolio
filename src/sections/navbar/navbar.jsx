767fa6de39f3ee40f636fb5038b8e45fa5a27bc3
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-19 13:01:06 -0300

    Logo

--- src/sections/navbar/navbar.jsx
+++ src/sections/navbar/navbar.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-
+import './navbar.scss'
 const navbar = () => {
   return (
     <nav id='nav'>
-      <h2>Logo</h2>
+      <div className='container nav__container'>
+        <a href='#' className='nav__logo'>
+          <img src='./images/Logo.jpg' alt='' />
+        </a>      
+      </div>
     </nav>
   )
 }
