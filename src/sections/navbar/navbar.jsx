c13b18d39d9df87c03ec1813413d5dc4a7e07fda
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-19 13:03:47 -0300

    logo via issue do github

--- src/sections/navbar/navbar.jsx
+++ src/sections/navbar/navbar.jsx
@@ -6,7 +6,7 @@ const navbar = () => {
     <nav id='nav'>
       <div className='container nav__container'>
         <a href='#' className='nav__logo'>
-          <img src='./images/Logo.jpg' alt='' />
+          <img src='https://user-images.githubusercontent.com/78994881/219959728-52fc0a6a-816a-46a7-9046-c699fbb36003.jpg' alt='' />
         </a>      
       </div>
     </nav>
