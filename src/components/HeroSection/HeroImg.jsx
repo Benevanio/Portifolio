7d1708c10c482607cac8283844192c5bcaa0eafc
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-20 18:17:57 -0300

    Pagina inicial(Component home), usando a imagen do anime fate

--- src/components/HeroSection/HeroImg.jsx
+++ src/components/HeroSection/HeroImg.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import './Hero.scss'
+import Logo from '../../IMG/Fundo.jpg'
+import ReactTypingEffect from 'react-typing-effect'
+import { Link } from 'react-router-dom'
 const HeroImg = () => {
   return (
-    <div>HeroImg</div>
+    <div className='hero'>
+      <div className='maks'>
+        <img src={Logo} alt='logo' className='IntroIm' />
+      </div>
+      <div className='content'>
+        <h1 className='title'>Bem vindo ao meu site</h1>
+        <p className='text'>
+          <ReactTypingEffect
+            text={["MERN Stack Developer", " Full Stack Developer ", "Front-End Developer ", "Back-End Developer"]}
+            speed={100}
+            eraseDelay={700}
+            typingDelay={1000}
+          />
+        </p>
+        <div>
+          <Link to='/project' className='btn01'>
+            <i className='fas fa-code'></i> Projects
+          </Link>
+          <Link to='/contact' className='btn02 btn-light'>
+            <i className='fas fa-phone'></i> Contact
+          </Link>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
