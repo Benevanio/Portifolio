44b8c8e358505334eec9931a3b559e82a1f7241e
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-21 09:26:11 -0300

    Adicionando a props na pagina de contato

--- src/section/Contact/Contact.jsx
+++ src/section/Contact/Contact.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from '../../components/navbar/navbar'
 import Footer from '../../components/Footer/Footer'
-
+import Projects from '../../components/Projects/Projects'
 const Contact = () => {
   return (
     <>
       <Navbar />
+      <Projects
+        Heading="Contact"
+        text="Get in touch with me"
+      />
       <Footer />
     </>
   )
