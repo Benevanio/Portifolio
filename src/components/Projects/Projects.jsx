659e3f358299db7f7f4e5fe4feb776395f00b790
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-21 09:06:23 -0300

    Iniciando a codificacao da pagina de estilos, e adicionando o bg(background rgba na pagina), Paginas modificadas : Projects e navbar.Dentro da section e da pasta componentes

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Project.scss'
+const Projects = () => {
+    return (
+        <div className="Hero__IMG">
+            <div className="Heading">
+                <h1>
+                    This is the Projects page
+                </h1>
+                <p>
+                    This is the Projects page
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Projects
--- src/section/Project/Project.jsx
+++ src/section/Project/Project.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../../components/navbar/navbar'
 import Footer from '../../components/Footer/Footer'
-
+import Projects from '../../components/Projects/Projects'
 const Project = () => {
   return (
     <>
       <Navbar />
+      <Projects />
       <Footer />
     </>
   )
