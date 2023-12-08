fa223a81d5564a8025fb13590c87ebd928d2b23a
Bene-Tesla <benevaniosantos930@gmail.com>
2023-12-08 13:06:54 -0300

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -60,8 +60,8 @@ const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>What I have done so far</p>
-        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+        <p className={styles.sectionSubText}>veja minha experiência:</p>
+        <h2 className={styles.sectionHeadText}>Experiência.</h2>
       </motion.div>
 
       <div className="mt-20 flex flex-col">
